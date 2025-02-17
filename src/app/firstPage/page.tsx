7f5d49abd2c679ece886238df1bf19ce25ccddf8
= <=>
2025-02-18 02:41:36 +0800

    landing

--- src/app/firstPage/page.tsx
+++ src/app/firstPage/page.tsx
@@ -3,7 +3,7 @@ import { SampleDiv } from "./_components/sample";
 export default function firstPage() {
   return (
     <div className="flex items-center gap-x-4">
-      this is the template comit
+      this is the landing comit
       <button className="rounded-md bg-blue-300 px-4 py-2 hover:bg-blue-100">
         B1
       </button>
